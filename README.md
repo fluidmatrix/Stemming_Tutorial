df1af31c0a101c77d713a898185743191a6f3c67
fluidmatrix <loner.mischief@gmail.com>
2025-12-14 18:13:55 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+This is a working example of Lab1 of Natural Language Processing Course
+offered on Coursera, which is taught by Professor Andrew NG, Professor Younes
+and Professor Lucasz.
+
+I have downloaded the tweets using the NLTK package, which is the open source
+package for Natural Language Processing. We have used a 50/50 split, which has 
+50% +ve and 50% -ve examples. 
+
+![Pie Chart](<c:/Users/saadr/OneDrive/Pictures/Screenshots 1/Screenshot 2025-12-14 180630.png>)
+I have developed pre-processor which would stem the original tweet, which could be
+fed to train the model. 
+
+Firstly, we have remove the punctuation and stopwords, then used 
+PorterStemmer from NLTK package to reduce the words to their basic 
+Stems. 
+
+
+At last, the print statement shows the original tweet 
+                    V/s
+The Stems which are stored in a list. 
+![Comparison ScrenShot](<c:/Users/saadr/OneDrive/Pictures/Screenshots 1/Screenshot 2025-12-14 175742.png>)
+
+I have also freezed the requirements to save space on my workstation
+as well you can install the dependencies using requirements.txt
+and replicate the tutorial.
---
+++ stemming.py
@@ -0,0 +1,86 @@
+import nltk                                # Python library for NLP
+from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
+import matplotlib.pyplot as plt            # library for visualization
+import random                              # pseudo-random number generator
+import re
+import string
+from nltk.corpus import stopwords          # module for stop words that come with NLTK
+from nltk.stem import PorterStemmer        # module for stemming
+from nltk.tokenize import TweetTokenizer   # module for tokenizing strings 
+
+# select the set of positive and negative tweets
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+# Now let us use Matplotlib to visualize the number of tweets that makes our dataset
+
+# Determinig the size of the plot
+fig = plt.figure(figsize=(4, 4))
+
+# Giving labels and title
+labels = 'Positive', 'Negative'
+
+# Size for each section of the pie chart
+sizes = [len(all_positive_tweets), len(all_negative_tweets)]
+
+# Declare pie chart, where the slices will be ordered and plotted counter-clockwise:
+plt.pie(sizes, labels=labels,  startangle=90)
+
+# Explicitly equalizing the aspect ratio
+plt.axis('equal')
+
+# Display the Chart
+plt.show()
+
+# Our selected sample. Complex enough to exemplify each step
+tweet = all_positive_tweets[2275]
+
+# remove old style retweet text "RT"
+tweet2 = re.sub(r'^RT[\s]+', '', tweet)
+
+# remove hyperlinks
+tweet2 = re.sub(r'https?://[^\s\n\r]+', '', tweet2)
+
+# remove hashtags
+# only removing the hash # sign from the word
+tweet2 = re.sub(r'#', '', tweet2)
+
+# instantiate tokenizer class
+tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+
+# tokenize tweets
+tweet_tokens = tokenizer.tokenize(tweet2)
+
+#Import the english stop words list from NLTK
+stopwords_english = stopwords.words('english') 
+
+# Instantiate an empty array
+tweets_clean = []
+
+for word in tweet_tokens: # Go through every word in your tokens list
+    if (word not in stopwords_english and  # remove stopwords
+        word not in string.punctuation):  # remove punctuation
+        tweets_clean.append(word)
+
+
+print()
+print('\033[92m')
+print(tweet)
+print('\033[94m')
+
+# Instantiate stemming class
+stemmer = PorterStemmer() 
+
+# Create an empty list to store the stems
+tweets_stem = [] 
+
+for word in tweets_clean:
+    stem_word = stemmer.stem(word)  # stemming word
+    tweets_stem.append(stem_word)  # append to the list
+
+print('stemmed words:')
+print(tweets_stem)
+
+
+
+
