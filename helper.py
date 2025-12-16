b72c435cc465915c57ecf01c095f4ba2821aa302
fluidmatrix <loner.mischief@gmail.com>
2025-12-16 19:06:56 +0530

    Updated Helper Functions, now you can use your own tweet to know if it is good or bad

--- helper.py
+++ helper.py
@@ -68,3 +68,145 @@ def build_freqs(tweets, ys):
                 freqs[pair] = 1
 
     return freqs
+
+
+def sigmoid(z): 
+    '''
+    Input:
+        z: is the input (can be a scalar or an array)
+    Output:
+        h: the sigmoid of z
+    '''
+    
+    # calculate the sigmoid of z
+    h = 1/(1 + np.exp(-z))
+    
+    return h
+
+def gradientDescent(x, y, theta, alpha, num_iters):
+    '''
+    Input:
+        x: matrix of features which is (m,n+1)
+        y: corresponding labels of the input matrix x, dimensions (m,1)
+        theta: weight vector of dimension (n+1,1)
+        alpha: learning rate
+        num_iters: number of iterations you want to train your model for
+    Output:
+        J: the final cost
+        theta: your final weight vector
+    '''
+    # get 'm', the number of rows in matrix x
+    m = x.shape[0]
+    
+    for i in range(0, num_iters):
+        
+        # get z, the dot product of x and theta
+        z = np.dot(x, theta)
+        
+        # get the sigmoid of z
+        h = sigmoid(z)
+        
+        # calculate the cost function
+        J = (-1/m)* np.sum(y * np.log(h) + (1-y) * np.log(1-h))
+        
+        gradient = (1 / m) * np.dot(x.T, (h - y))
+
+        # update the weights theta
+        theta = theta - alpha * gradient
+        
+    J = float(J)
+    return J, theta
+
+
+def extract_features(tweet, freqs, process_tweet=process_tweet):
+    '''
+    Input: 
+        tweet: a string containing one tweet
+        freqs: a dictionary corresponding to the frequencies of each tuple (word, label)
+    Output: 
+        x: a feature vector of dimension (1,3)
+    '''
+    # process_tweet tokenizes, stems, and removes stopwords
+    word_l = process_tweet(tweet)
+    
+    # 3 elements for [bias, positive, negative] counts
+    x = np.zeros(3) 
+    
+    # bias term is set to 1
+    x[0] = 1 
+    
+    
+    # loop through each word in the list of words
+    for word in word_l:
+        
+        # increment the word count for the positive label 1
+        x[1] += freqs.get((word,1),0)
+        
+        # increment the word count for the negative label 0
+        x[2] += freqs.get((word,0),0)
+        
+    
+    x = x[None, :]  # adding batch dimension for further processing
+    assert(x.shape == (1, 3))
+    return x
+
+
+def predict_tweet(tweet, freqs, theta):
+    '''
+    Input: 
+        tweet: a string
+        freqs: a dictionary corresponding to the frequencies of each tuple (word, label)
+        theta: (3,1) vector of weights
+    Output: 
+        y_pred: the probability of a tweet being positive or negative
+    '''
+    
+    # extract the features of the tweet and store it into x
+    x = extract_features(tweet, freqs)
+    
+    # make the prediction using x and theta
+    y_pred = sigmoid(np.dot(x, theta))
+    
+    
+    return y_pred
+
+def test_logistic_regression(test_x, test_y, freqs, theta, predict_tweet=predict_tweet):
+    """
+    Input: 
+        test_x: a list of tweets
+        test_y: (m, 1) vector with the corresponding labels for the list of tweets
+        freqs: a dictionary with the frequency of each pair (or tuple)
+        theta: weight vector of dimension (3, 1)
+    Output: 
+        accuracy: (# of tweets classified correctly) / (total # of tweets)
+    """
+    
+    
+    # the list for storing predictions
+    y_hat = []
+    
+    for tweet in test_x:
+        # get the label prediction for the tweet
+        y_pred = predict_tweet(tweet, freqs, theta)
+        
+        if y_pred > 0.5:
+            # append 1.0 to the list
+            y_hat.append(1.0)
+        else:
+            # append 0 to the list
+            y_hat.append(0.0)
+
+    # With the above implementation, y_hat is a list, but test_y is (m,1) array
+    # convert both to one-dimensional arrays in order to compare them using the '==' operator
+    y_hat = np.array(y_hat)
+    test_y = np.array(test_y)
+    accuracy = np.mean(y_hat == test_y)
+
+    
+    return accuracy
+
+
+
+
+
+
